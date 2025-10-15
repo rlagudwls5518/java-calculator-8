1843a5c31f2bcfedb7ca1cb586395a2be2840be6
rlagudwls5518 <rlagudwls5518@naver.com>
2025-10-15 22:10:43 +0900

    테스트 1,2번 돌아가는 코드 완성

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,52 +8,56 @@ public class Calculator {
     static ArrayList<Integer> Numlist = new ArrayList<>();
 
     public static void Run() {
-        inputData();
-        dataSum();
+        InputData();
+        Sum();
     }
 
     //입력하는 메서드
-    private static void inputData() {
+    private static void InputData() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        inputSeperater();
+        InputSeperater();
     }
 
     //사용자가 입력하고 구분자를 기준으로 나누는 메서드
-    private static void inputSeperater() {
+    private static void InputSeperater() {
         inputDataArray = Console.readLine().split("[,:]");
-        eceptionError(inputDataArray);
-        findNum(inputDataArray);
+        FindNum(inputDataArray);
+        EceptionError(inputDataArray, Numlist);
     }
 
     //구분자를 기준으로 분리한 배열에서 숫자만 추출하는 메서드
-    private static void findNum(String[] string) {
-        for (String s : string) {
-            int dataNumber = Integer.parseInt(s.replaceAll("[^0-9]", ""));
+    private static void FindNum(String[] strings) {
+        for (String s : strings) {
+            int dataNumber = Integer.parseInt(s.replaceAll("[^0-9-]", ""));
             //숫자를 추출했는데 배열에 어떻게 넣을 것인가??
             Numlist.add(dataNumber);
         }
     }
 
     //데이터 합치는 메서드
-    private static void dataSum() {
+    private static void Sum() {
         int sum = 0;
 
-        for (int a : Numlist) {
-            sum += a;
+        for (int num : Numlist) {
+            System.out.println(num);
+            sum += num;
         }
         System.out.println("결과 : " + sum);
     }
 
     //예외처리
-    private static void eceptionError(String[] strings) {
+    private static void EceptionError(String[] strings, ArrayList<Integer> list) {
 
         for (String s : strings) {
             if (s == null || s.isBlank()) {
                 throw new IllegalArgumentException("문자열은 null 또는 공백일 수 없습니다.");
             }
-//            if (Integer.parseInt(s) < 0) {
-//                throw new IllegalArgumentException("0이하는 안됩니다");
-//            }
+        }
+        
+        for (int a : list) {
+            if (a < 0) {
+                throw new IllegalArgumentException("음수는 안됩니다");
+            }
         }
     }
 }
