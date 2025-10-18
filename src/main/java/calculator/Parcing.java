c54d22ce3e31619d0c822d8f8d42b6d16d329406
rlagudwls5518 <rlagudwls5518@naver.com>
2025-10-18 13:49:25 +0900

    feat(Parcing) : 음수 예외처리 추가

--- src/main/java/calculator/Parcing.java
+++ src/main/java/calculator/Parcing.java
@@ -1,6 +1,5 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 
 public class Parcing {
@@ -9,17 +8,37 @@ public class Parcing {
     private static String determine = "[,:]";
 
     //사용자가 입력하고 구분자를 기준으로 나누는 메서드
-    public static void InputSeperater() {
-        inputDataArray = Console.readLine().split(determine);
+    public static void InputSeperater(String s) {
+        ExceptionStringEmpty(s);
+
+        inputDataArray = s.split(determine);
+
         FindNum(inputDataArray);
-        Calculator.EceptionError(inputDataArray, Numlist);
+        EceptionError(inputDataArray, Numlist);
     }
 
     //구분자를 기준으로 분리한 배열에서 숫자만 추출하는 메서드
-    public static void FindNum(String[] strings) {
+    private static void FindNum(String[] strings) {
         for (String s : strings) {
-            int dataNumber = Integer.parseInt(s.replaceAll("[^0-9-]", ""));
+            String cleaned = s.replaceAll("[^0-9-]", "");
+            int dataNumber = Integer.parseInt(cleaned);
             Numlist.add(dataNumber);
+        }
+    }
+
+    private static void ExceptionStringEmpty(String s) {
+        if (s.trim().isEmpty()) {
+            //System.out.println("결과 : 0");
+            throw new IllegalArgumentException("공백입니다");
+        }
+
+    }
+
+    private static void EceptionError(String[] strings, ArrayList<Integer> list) {
+        for (int a : list) {
+            if (a < 0) {
+                throw new IllegalArgumentException("음수는 안됩니다");
+            }
         }
     }
 }
