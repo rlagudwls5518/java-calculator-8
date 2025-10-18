5e2d6c82d07883e6b0140f3ec74b6f65a5d7b390
rlagudwls5518 <rlagudwls5518@naver.com>
2025-10-18 12:51:42 +0900

    feat(Parcing) : 구분자 나누는메서드, 숫자추출 메서드 클래스분리

---
+++ src/main/java/calculator/Parcing.java
@@ -0,0 +1,25 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+
+public class Parcing {
+    static String[] inputDataArray;
+    static ArrayList<Integer> Numlist = new ArrayList<>();
+    static String determine = "[,:]";
+    
+    //사용자가 입력하고 구분자를 기준으로 나누는 메서드
+    public static void InputSeperater() {
+        inputDataArray = Console.readLine().split(determine);
+        FindNum(inputDataArray);
+        Calculator.EceptionError(inputDataArray, Numlist);
+    }
+
+    //구분자를 기준으로 분리한 배열에서 숫자만 추출하는 메서드
+    public static void FindNum(String[] strings) {
+        for (String s : strings) {
+            int dataNumber = Integer.parseInt(s.replaceAll("[^0-9-]", ""));
+            Numlist.add(dataNumber);
+        }
+    }
+}
