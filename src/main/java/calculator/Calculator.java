3dfe8e830a7f19d065063b36e990b679b2e06eb5
rlagudwls5518 <rlagudwls5518@naver.com>
2025-10-18 13:49:55 +0900

    feat(Carculator) : 입력메서드 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,6 +1,6 @@
 package calculator;
 
-import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
 
@@ -13,7 +13,8 @@ public class Calculator {
     //입력하는 메서드
     private static void InputData() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        Parcing.InputSeperater();
+        String inputString = Console.readLine();
+        Parcing.InputSeperater(inputString);
     }
 
     //데이터 합치는 메서드
@@ -27,18 +28,4 @@ public class Calculator {
         System.out.println("결과 : " + sum);
     }
 
-    //예외처리
-    public static void EceptionError(String[] strings, ArrayList<Integer> list) {
-
-        for (String s : strings) {
-            if (s == null || s.isBlank()) {
-                throw new IllegalArgumentException("문자열은 공백일 수 없습니다.");
-            }
-        }
-        for (int a : list) {
-            if (a < 0) {
-                throw new IllegalArgumentException("음수는 안됩니다");
-            }
-        }
-    }
 }
