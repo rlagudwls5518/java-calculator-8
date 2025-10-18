def6af7f15aae9b8e5af7fc4a551d99e6430321a
rlagudwls5518 <rlagudwls5518@naver.com>
2025-10-18 21:01:56 +0900

    refactor : InputTest, InputData, ExceptionStringEmpty메서드 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class Calculator {
 
@@ -10,10 +11,22 @@ public class Calculator {
         Sum();
     }
 
+    private static String InputTest() {
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException e) {
+            return null;
+        }
+    }
+
     //입력하는 메서드
     private static void InputData() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String inputString = Console.readLine();
+        String inputString = InputTest();
+
+        if (ExceptionStringEmpty(inputString)) {
+            return;
+        }
         Parcing.InputSeperater(inputString);
     }
 
@@ -26,6 +39,17 @@ public class Calculator {
             sum += num;
         }
         System.out.println("결과 : " + sum);
+    }
+
+    //입력 없거나 빈문자열일때
+    private static boolean ExceptionStringEmpty(String s) {
+        if (s == null) {
+            return true;
+        }
+        if (s.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        return false;
     }
 
 }
